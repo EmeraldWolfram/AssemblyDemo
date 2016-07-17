293449414cd2d72f1ae86db8e2904236bf0b3ec1
EmeraldWolfram <stevenfgy@yahoo.com>
2016-07-17 14:53:28 +0800

    Removed spaces in SaveRegisters.s

--- src/SaveRegisters.s
+++ src/SaveRegisters.s
@@ -1,23 +1,22 @@
-								PRESERVE8
-                THUMB
+					PRESERVE8
+					THUMB
+					AREA    MyData, DATA, READWRITE, ALIGN=2
+					EXPORT  task1Sp
+task1Sp		DCD     1
 
-                AREA    MyData, DATA, READWRITE, ALIGN=2
-                EXPORT  task1Sp
-task1Sp					DCD     1
-
-                AREA    MyCode, CODE, READONLY
-saveRegs		  	PROC
-                EXPORT  saveRegs
-									
-								LDR			r13, =task1Sp		;Let SP point to 'task1Sp'
-								LDR			r13, [r13]			;Load value of 'task1Sp' into SP
-								PUSH		{r0, r1, r2, r3}
-								PUSH		{r4, r5, r6, r7}
-								PUSH		{r8, r9, r10, r11}
-								PUSH		{r12, r14}
-
-								BX			lr									; Return to caller
+					AREA		MyCode, CODE, READONLY
+saveRegs	PROC
+					EXPORT  saveRegs
 								
-                ENDP
-									
-								END
+					LDR			r13, =task1Sp		;Let SP point to 'task1Sp'
+					LDR			r13, [r13]			;Load value of 'task1Sp' into SP
+					PUSH		{r0, r1, r2, r3}
+					PUSH		{r4, r5, r6, r7}
+					PUSH		{r8, r9, r10, r11}
+					PUSH		{r12, r14}
+
+					BX			lr									; Return to caller
+								
+					ENDP
+								
+					END
